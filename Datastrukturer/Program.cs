b3f074e2297dcc89e321cff7dc13f3292e822e80
AFitBear <118851426+AFitBear@users.noreply.github.com>
2023-01-16 21:43:26 +0100

    Update Program.cs

--- Datastrukturer/Program.cs
+++ Datastrukturer/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Datastrukturer
+namespace Datastrukturer
 {
     internal class Program
     {
@@ -12,11 +12,14 @@
             string soeg = "word";
             Hash_1_1(soeg); //laver en hash-tabel og søger
             Console.WriteLine();
-        } 
+            //Opgave 1 med stack er svaret bare 4.
+            //Opgave 2
+            Stack_2();
+        }
         static private (bool, string[]) Op_3(int indx, string tilsat, string[] strang)
         {
             string[] temp = new string[strang.Length + 1];
-            if (indx < temp.Length && indx !<0)
+            if (indx < temp.Length && indx! < 0)
             {
                 for (int i = 0; i < temp.Length; i++)
                 {
@@ -31,7 +34,7 @@
                     }
                     else
                     {
-                        temp[i] = strang[i-1];
+                        temp[i] = strang[i - 1];
                     }
                 }
                 strang = temp;
@@ -43,9 +46,25 @@
         {
             Dictionary<string, string> MyDict = new
             Dictionary<string, string>() { { "blender file","blend" }, { "word","doxc" }, { "application","exe" }, { "excel","xlsx" },
-            { "text","txt" }, { "jpg file","jpg" }, { "png file","png" } ,{ "gif file","gif" }, { "powerpint","pptx" }, { "maple","mpl" } } ;
+            { "text","txt" }, { "jpg file","jpg" }, { "png file","png" } ,{ "gif file","gif" }, { "powerpint","pptx" }, { "maple","mpl" } };
             Console.WriteLine(MyDict[soeg]);
             return MyDict[soeg];
         }
+        static private void Stack_2()
+        {
+            //string snakk = { 'S', 'n', 'a', 'k', 'k', 'e', ' ', 'b', 'a', 'g', 'v', 'e', 'n', 'd' };
+            string snakk = "Snakke bagvend";
+            Stack<char> myStack = new Stack<char>();
+            for (int i = 0; i < snakk.Length; i++)
+            {
+                myStack.Push(snakk[i]);
+            }
+            for (int i = 0; i < myStack.Count; i++)
+            {
+                Console.Write(myStack.Peek);
+                Console.WriteLine(myStack.Pop());
+            }
+        }
+            
     }
 }
