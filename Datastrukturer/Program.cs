580eff1bd695742b7df1c31e505e6f7e49d09cab
AFitBear <118851426+AFitBear@users.noreply.github.com>
2023-01-19 19:25:21 +0100

    Update Program.cs
    Done

--- Datastrukturer/Program.cs
+++ Datastrukturer/Program.cs
@@ -1,9 +1,23 @@
-namespace Datastrukturer
+using System.Diagnostics;
+
+namespace Datastruktur
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Opgave 1
+            string[] mitArray1 = { "sovs", "bacon", "kartofler", "chips", "cocio", "vand", "burger" };
+            string ordd = "chips";
+            Op1_1(mitArray1, ordd);
+            //Opgave 2 med metoder
+            string[] mitArray = { "sovs", "bacon", "kartofler", "chips", "cocio", "vand", "burger" };
+            int deleteAt = 1;
+            Op1_2(mitArray, deleteAt);
+            //Opgave 1 i liste
+            ListOp1();
+            //Opgave 2 i list
+            ListOp2();
             int indx = 3;
             string[] strang = { "kage", "sovs", "pap", "ur", "tv", "lys", "led" };
             string tilsat = "ord";
@@ -13,8 +27,63 @@ namespace Datastrukturer
             Hash_1_1(soeg); //laver en hash-tabel og søger
             Console.WriteLine();
             //Opgave 1 med stack er svaret bare 4.
-            //Opgave 2
+            //Opgave 2 med stack
             Stack_2();
+            //Svaret på opgave queue. er 2.
+        }
+        static private int Op1_1(string[] mitArray1, string ordd)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < mitArray1.Length; i++)
+            {
+                if (mitArray1[i] == ordd)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static (string[], bool) Op1_2(string[] mitArray, int deleteAt)
+
+        {
+            string[] tempArray = new string[mitArray.Length - 1];
+            if (mitArray.Length > deleteAt)
+            {
+                for (int i = 0; i < tempArray.Length; i++)
+                {
+                    if (i < deleteAt)
+                    {
+                        tempArray[i] = mitArray[i];
+                    }
+                    else
+                    {
+                        tempArray[i] = mitArray[i + 1];
+                    }
+                }
+
+                mitArray = tempArray;
+                return (mitArray, true);
+            }
+            else
+            {
+                return (mitArray, false);
+            }
+        }
+        static private void ListOp1()
+        {
+            List<int> talList = new List<int>() { 10, 20, 30 };
+            talList.Add(40);
+            talList.Add(15);
+            talList.Sort();
+            talList.Reverse();
+            //Den sætter 40 i slutningen af listen. så sætter den 15 i slutningen. så sortere den. så går den omvendt.
+        }
+        static private void ListOp2()
+        {
+            List<char> snakkeList = new List<char>() { 'S', 'n', 'a', 'a', 'k', 'k', 'e',' ', 'b', 'a', 'g', 'v', 'e', 'n', 'd', 't' };
+            snakkeList.Reverse();
+            for (int i = 0; i < snakkeList.Count; i++)
+                Console.WriteLine(snakkeList[i].ToString());
         }
         static private (bool, string[]) Op_3(int indx, string tilsat, string[] strang)
         {
@@ -59,12 +128,10 @@ namespace Datastrukturer
             {
                 myStack.Push(snakk[i]);
             }
-            for (int i = 0; i < myStack.Count; i++)
+            for (int i = myStack.Count; i > 0; i--)
             {
-                Console.Write(myStack.Peek);
                 Console.WriteLine(myStack.Pop());
             }
         }
-            
     }
-}
+} //yes
